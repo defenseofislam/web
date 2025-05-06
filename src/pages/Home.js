b98899889701a65e9ca95d02490ea4bbdd0cbdeb
KK <kk@kk12345defenseoi.com>
2025-05-06 22:08:29 +0530

    feat: add Updates component for latest articles and refactor Home component to utilize it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,10 @@
 import React from "react";
 import { Layout, Typography, Button, Card, List } from "antd";
 import { Link } from "react-router-dom";
-
+import updates from "../components/Updates";
 const { Content } = Layout;
 const { Title, Paragraph, Text } = Typography;
 
-const updates = [
-  {
-    title:
-      "1. New Article on Darwinism: Darwinian Evolution hypothesis is better than believeing in Creationism Refutation",
-    date: "Published on 20:47:11 IST May 6, 2025",
-    link: "/darwinism/dn-vs-ct/1",
-  },
-  {
-    title:
-      "2. New Article on Islam: Age Of Mother Ayesha (RA) at the time of marriage",
-    date: "Published on 20:13:15 IST May 6, 2025",
-    link: "/islam/hadith/1",
-  },
-  {
-    title:
-      "3. New Aricle on Islam: Is Qur'an word of God?",
-    date: "Published on 19:33:12 IST May 6, 2025",
-    link: "/islam/quran/1",
-  },
-  {
-    title: "4. New Aricle on Islam: Qur'an Plagiarism Claims Refutation ",
-    date: "Published on 19:42:12 IST May 6, 2025",
-    link: "/islam/quran/2",
-  },
-];
 
 function Home() {
   return (
