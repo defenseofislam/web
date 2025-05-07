b6b34f89ae360f9224c328c492cbee7bfa4b6ff6
KK <kk@kk12345defenseoi.com>
2025-05-07 12:49:41 +0530

    feat: update article titles and correct numbering in Updates component

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -1,25 +1,25 @@
 const updates = [
   {
-    title: "1.Updated article : Allegations of plagiarism against the Quran",
+    title: "1.New article on Agnosticism : I believe in God but I don't follow religion : Refuted",
+    date: "Published on 12:49:12 IST May 7, 2025",
+    link: "/agnosticism/as-th-de/1",
+  },
+  {
+    title: "2.Updated article : Allegations of plagiarism against the Quran",
     date: "Published on 09:12:12 IST May 7, 2025",
     link: "/islam/quran/2",
   },
   {
-    title: "2. Updated article : Is Qur'an word of God?",
+    title: "3. Updated article : Is Qur'an word of God?",
     date: "Published on 08:57:21 IST May 7, 2025",
     link: "/islam/quran/1",
   },
   {
     title:
-      "3. New Article on Darwinism: Darwinian Evolution hypothesis is better than believeing in Creationism Refutation",
+      "4. New Article on Darwinism: Darwinian Evolution hypothesis is better than believeing in Creationism Refutation",
     date: "Published on 20:47:11 IST May 6, 2025",
     link: "/darwinism/dn-vs-ct/1",
-  },
-  {
-    title: "4. New Aricle on Islam: Is Qur'an word of God?",
-    date: "Published on 19:33:12 IST May 6, 2025",
-    link: "/islam/quran/1",
-  },
+  }
 ];
 
 export default updates;
