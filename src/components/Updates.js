ec4ae2dc5809fea6386c6fac012c9bec87e972c4
KK <kk@kk12345defenseoi.com>
2025-05-08 10:25:26 +0530

    feat: update article titles and reorder entries in Updates.js for clarity

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -1,25 +1,24 @@
 const updates = [
   {
-    title: "1.New article on Agnosticism : I believe in God but I don't follow religion : Refuted",
+    title: "1.New article on Miscellaneous :Kashmir belongs to whom? India or Pakistan?",
+    date: "Published on 10:25:18 IST May 8, 2025",
+    link: "/misc/gt/1",
+  },
+  {
+    title: "2.New article on Agnosticism : I believe in God but I don't follow religion : Refuted",
     date: "Published on 12:49:12 IST May 7, 2025",
     link: "/agnosticism/as-th-de/1",
   },
   {
-    title: "2.Updated article : Allegations of plagiarism against the Quran",
+    title: "3.Updated article : Allegations of plagiarism against the Quran",
     date: "Published on 09:12:12 IST May 7, 2025",
     link: "/islam/quran/2",
   },
   {
-    title: "3. Updated article : Is Qur'an word of God?",
+    title: "4. Updated article : Is Qur'an word of God?",
     date: "Published on 08:57:21 IST May 7, 2025",
     link: "/islam/quran/1",
   },
-  {
-    title:
-      "4. New Article on Darwinism: Darwinian Evolution hypothesis is better than believeing in Creationism Refutation",
-    date: "Published on 20:47:11 IST May 6, 2025",
-    link: "/darwinism/dn-vs-ct/1",
-  }
 ];
 
 export default updates;
