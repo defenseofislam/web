a1369060d90118e6be958a070c31a910de7f9d7d
KK <kk@kk12345defenseoi.com>
2025-05-08 11:50:20 +0530

    feat: refactor Articles component to import article data from a separate file

---
+++ src/components/ArticleList.js
@@ -0,0 +1,41 @@
+const allData = [
+  {
+    key: 1,
+    name: "Is the Quran the Word of God? A Scholarly Examination",
+    date: "Published on 12:25:18 IST May 6, 2025",
+    link: "islam/quran/1",
+  },
+  {
+    key: 2,
+    name: "Debunking the Claims of Plagiarism Against the Quran",
+    date: "Published on 14:25:18 IST May 6, 2025",
+    link: "islam/quran/2",
+  },
+  {
+    key: 3,
+    name: "How Old Was Ayesha (R.A.) at the Time of Her Marriage? A Historical and Theological Analysis ",
+    date: "Published on 10:25:18 IST May 6, 2025",
+    link: "islam/hadith/1",
+  },
+
+  {
+    key: 4,
+    name: " I am deist - I believe in God but not in any religion Refutation",
+    date: "Published on 18:15:23 IST May 7, 2025",
+    link: "agnosticism/as-th-de/1",
+  },
+  {
+    key: 5,
+    name: "Darwinian Evolution hypothesis is better than believeing in Creationism Refutation",
+    date: "Published on 19:12:12 IST May 7, 2025",
+    link: "darwinism/dn-vs-ct/1",
+  },
+  {
+    key: 6,
+    name: "Kashmir Belongs to Whom ? India or Pakistan??",
+    date: "Published on 10:25:18 IST May 8, 2025",
+    link: "misc/gt/1",
+  },
+];
+
+export default allData;
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -3,56 +3,15 @@ import { Table, Button } from "antd";
 import { LinkOutlined } from "@ant-design/icons";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useState } from "react";
-
+import allData from "../components/ArticleList";
 const Articles = () => {
-  const allData = [
-    {
-        "key":1,
-        "name":"Is the Quran the Word of God? A Scholarly Examination",
-        "date":"Published on 12:25:18 IST May 6, 2025",
-        "link":"islam/quran/1"
-    },
-    {
-        "key":2,
-        "name":"Debunking the Claims of Plagiarism Against the Quran",
-        "date":"Published on 14:25:18 IST May 6, 2025",
-        "link":"islam/quran/2"
-    },
-    {
-        "key":3,
-        "name":"How Old Was Ayesha (R.A.) at the Time of Her Marriage? A Historical and Theological Analysis ",
-        "date":"Published on 10:25:18 IST May 6, 2025",
-        "link":"islam/hadith/1"
-    },
-
-    {
-        "key":4,
-        "name":" I am deist - I believe in God but not in any religion Refutation",
-        "date":"Published on 18:15:23 IST May 7, 2025",
-        "link":"agnosticism/as-th-de/1"
-    },
-    {
-        "key":5,
-        "name":"Darwinian Evolution hypothesis is better than believeing in Creationism Refutation",
-        "date":"Published on 19:12:12 IST May 7, 2025",
-        "link":"darwinism/dn-vs-ct/1"
-    },
-    {
-        "key":6,
-        "name":"Kashmir Belongs to Whom ? India or Pakistan??",
-        "date":"Published on 10:25:18 IST May 8, 2025",
-        "link":"misc/gt/1"
-    },
-
-  ]
-
   const [visibleData, setVisibleData] = useState(allData.slice(0, 10));
   const [hasMore, setHasMore] = useState(true);
   // Check if mobile device
   const isMobile =
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  );
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
 
   const fetchMoreData = () => {
     setTimeout(() => {
@@ -69,22 +28,21 @@ const Articles = () => {
 
   const columns = [
     {
-        title: "Article Name",
-        dataIndex: "name",
-        key: "name",
-        render: (text) => (
-            <div style={{ whiteSpace: 'normal', wordBreak: 'break-word' }}>
-              <strong>{text}</strong>
-            </div>
-          ),
-        width:!isMobile?"80%":"70%",
-               
-      },
+      title: "Article Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => (
+        <div style={{ whiteSpace: "normal", wordBreak: "break-word" }}>
+          <strong>{text}</strong>
+        </div>
+      ),
+      width: !isMobile ? "80%" : "70%",
+    },
     {
       title: "Date Published",
       dataIndex: "date",
       key: "date",
-      width:isMobile?"10%":"30%",
+      width: isMobile ? "10%" : "30%",
     },
     {
       title: "Read",
@@ -110,12 +68,16 @@ const Articles = () => {
       >
         <div
           id="scrollableDiv"
-          style={{ maxHeight: !isMobile?"70vh":"50vh", overflowY: "auto",width: "100%" }}
+          style={{
+            maxHeight: !isMobile ? "70vh" : "50vh",
+            overflowY: "auto",
+            width: "100%",
+          }}
         >
           <Table
             columns={columns}
             dataSource={visibleData}
-            pagination={false} 
+            pagination={false}
             bordered
             size="middle"
           />
