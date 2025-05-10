5de44a175c8c76477de83bd5fc4daac651861348
KK <kk@kk12345defenseoi.com>
2025-05-10 18:43:28 +0530

    fix: update article titles for consistency and remove unnecessary numbering

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -1,23 +1,23 @@
 const updates = [
   {
-    title: "1.New article on Miscellaneous :Kashmir belongs to whom? India or Pakistan?",
+    title: `New article on Miscellaneous :"Not All Muslims Are Terrorists, But All Terrorists Are Muslims"`,
+    date: "Published on 10:25:18 IST May 8, 2025",
+    link: "/misc/gt/2",
+  },
+  {
+    title: "New article on Miscellaneous :Kashmir belongs to whom? India or Pakistan?",
     date: "Published on 10:25:18 IST May 8, 2025",
     link: "/misc/gt/1",
   },
   {
-    title: "2.New article on Agnosticism : I believe in God but I don't follow religion : Refuted",
+    title: "New article on Agnosticism : I believe in God but I don't follow religion : Refuted",
     date: "Published on 12:49:12 IST May 7, 2025",
     link: "/agnosticism/as-th-de/1",
   },
   {
-    title: "3.Updated article : Allegations of plagiarism against the Quran",
+    title: "Updated article : Allegations of plagiarism against the Quran",
     date: "Published on 09:12:12 IST May 7, 2025",
     link: "/islam/quran/2",
-  },
-  {
-    title: "4. Updated article : Is Qur'an word of God?",
-    date: "Published on 08:57:21 IST May 7, 2025",
-    link: "/islam/quran/1",
   },
 ];
 
